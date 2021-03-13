8c6cc2b20b2d775c58a2608b5850460665ea10ec
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-03-13 13:23:22 -0300

    Finaliza programa Gritador.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine(" Olá usuário! \n Como está se sentindo hoje?");
+            string Texto = Console.ReadLine();
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine(  Texto.ToUpper());
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(" Nossa!!! \n Por que você está bravo?");
+            
+            
+        
+        }
+    }
+}
